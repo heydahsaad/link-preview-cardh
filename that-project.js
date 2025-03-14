5ec37d9e2438afd1f51b73967f932e1d045e1415
hedahsaad <haidahsaad@gmail.com>
2025-03-14 10:58:37 -0400

    made sure the fetch api worked

--- that-project.js
+++ that-project.js
@@ -21,15 +21,21 @@ export class ThatProject extends DDDSuper(I18NMixin(LitElement)) {
   constructor() {
     super();
     this.title = "";
+    this.webLink = "";    
+    this.imageLink = "";
+    this.value = null;
+    this.loading = false;
+
     this.t = this.t || {};
     this.t = {
       ...this.t,
       title: "Title",
     };
+    
     this.registerLocalization({
       context: this,
       localesPath:
-        new URL("./locales/that-project.ar.json", import.meta.url).href +
+        new URL("./locales/link-preview-card.ar.json", import.meta.url).href +
         "/../",
       locales: ["ar", "es", "hi", "zh"],
     });
@@ -40,6 +46,9 @@ export class ThatProject extends DDDSuper(I18NMixin(LitElement)) {
     return {
       ...super.properties,
       title: { type: String },
+      webLink: { type: String, attribute: "web-link" },
+      description: { type: String },
+      imageLink: { type: String, attribute: "image-link" },
     };
   }
 
@@ -60,17 +69,67 @@ export class ThatProject extends DDDSuper(I18NMixin(LitElement)) {
       h3 span {
         font-size: var(--that-project-label-font-size, var(--ddd-font-size-s));
       }
+      img {
+          display: block;
+          max-width: 400px;
+          max-height: 400px;
+          height: auto;
+          margin: var(--ddd-spacing-2);
+        }
     `];
   }
 
-  // Lit render the HTML
-  render() {
-    return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+
+  updated(changedProperties) {
+    if (changedProperties.has("webLink")) {
+      this.updateResults();
+    }
   }
+
+updateResults(value) {
+  this.loading = true;
+  fetch(
+    `https://corsproxy.io/?url=https://open-apis.hax.cloud/api/services/website/metadata?q=${this.webLink}`
+  )
+    .then((d) => (d.ok ? d.json() : {}))
+    .then((response) => {
+      if (response.data["og:title"]) {
+        this.title = response.data["og:title"];
+      }
+      if (response.data["og:description"]) {
+        this.description = response.data["og:description"];
+      }
+      if (response.data["description"]) {
+        this.description = response.data["description"];
+      }
+      if (response.data["og:image"]) {
+        this.imageLink = response.data["og:image"];
+      } else if (response.data["ld+json"].logo) {
+        this.imageLink = response.data["ld+json"].logo;
+      } else if (response.data["og:title"]) {
+        this.imageLink = response.data["ld+json"]["publisher"].logo;
+      }
+    });
+}
+
+render() {
+  return html`
+    <div class="wrapper">
+      <h2>${this.title}</h2>
+      <!-- <p><a href="${this.webLink}" target="_blank">${this.webLink}</a></p> -->
+      <p>${this.description}</p>
+      <img
+        src="${this.imageLink}"
+        alt="${this.t.title}: ${this.title}"
+        loading="lazy"
+        width="100%"
+      />
+      <slot></slot>
+    </div>
+  <!-- <details open></details> -->
+  `;
+}
+
 
   /**
    * haxProperties integration via file reference
